b5d8ba8ca985b4c6be911a39bbd88302f8bcd81a
Ivan Jester Echon <jesterivan4@gmail.com>
2026-03-09 21:29:47 +0800

    Added Services Section and fixed some alignment and minor issues.

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
         <h2 className='text-center text-5xl font-Ovo'>About me</h2>
 
         <div className='flex w-full flex-col lg:flex-row items-center gap-20 my-20'>
-            <div className='w-64 sm:w-80 rounded-3xl max-w-none'>
+            <div className='w-64 sm:w-80 rounded-3xl max-w-none flex-1'>
                 <Image src={assets.user_image} alt='user' className='w-full rounded-3xl' />
             </div>
             <div className='flex-1'>
@@ -18,7 +18,7 @@ const About = () => {
                 </p>
                 <ul className='grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl'>
                     {infoList.map(({icon, iconDark, title, description}, index) => (
-                        <li key={index} className='border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 hover:shadow-black'>
+                        <li key={index} className='border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 hover:shadow-(--box-shadow-black)'>
                             <Image src={icon} alt={title}/>
                             <h3 className='my-4 font-semibold text-gray-700 font-Outfit'>{title}</h3>
                             <p>{description}</p>
@@ -29,7 +29,7 @@ const About = () => {
                 <ul className='flex items-center gap-3 sm:gap-5'>
                     {toolsData.map((tool, index) => (
                         <li className='flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500 ' key={index} >
-                            <Image src={tool} alt={tool.name} className='w-5 sm:w-7'/>
+                            <Image src={tool} alt='' className='w-5 sm:w-7'/>
                             <span>{tool.name}</span>
                         </li>
                     ))}
