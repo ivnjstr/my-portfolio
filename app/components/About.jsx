33a28dedce2cbb285ab70ee8a1dd021eed216426
Ivan Jester Echon <jesterivan4@gmail.com>
2026-03-10 20:55:05 +0800

    Created and designed a Work section and adjusted the main user image in about section

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
         <h2 className='text-center text-5xl font-Ovo'>About me</h2>
 
         <div className='flex w-full flex-col lg:flex-row items-center gap-20 my-20'>
-            <div className='w-64 sm:w-80 rounded-3xl max-w-none flex-1'>
+            <div className='w-80 sm:w-80 rounded-3xl max-w-none flex-1'>
                 <Image src={assets.user_image} alt='user' className='w-full rounded-3xl' />
             </div>
             <div className='flex-1'>
