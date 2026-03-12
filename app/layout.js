61b7399e49ecda4415b06f7740629ddd72f04e9d
Ivan Jester Echon <jesterivan4@gmail.com>
2026-03-12 21:46:43 +0800

    Added dark mode functionality. Fixed the dark mode theme for the navigation section.

--- app/layout.js
+++ app/layout.js
@@ -18,10 +18,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
       <body
-        className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden`}
+        className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden dark:bg-darkTheme dark:text-white`}
       >
         {children}
       </body>
     </html>
+
+    // The `dark` class is added to the <html> element to enable dark mode styles defined in the CSS.
+    // dark:bg-darkTheme dark:text-white works when the `dark` class is present on the <html> element, applying the specified background and text colors for dark mode.
   );
 }
