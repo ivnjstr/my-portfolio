24ba071e421799e27e00260e99c724d34885e824
Ivan Jester Echon <jesterivan4@gmail.com>
2026-03-13 21:51:24 +0800

    Added animation scroll for each sections using motion.dev

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,42 +1,83 @@
 import { assets, infoList, toolsData } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
+import { motion } from "motion/react"
 
 const About = ({isDarkMode}) => {
   return (
-    <div id='about' className='w-full px-[12%] py-10 scroll-mt-20'>
-        <h4 className='text-center mb-2 text-lg font-Ovo'>Introduction</h4>
-        <h2 className='text-center text-5xl font-Ovo'>About me</h2>
+    <motion.div
+    initial={{opacity: 0}}
+    whileInView={{opacity: 1}}
+    transition={{duration: 1}}
+    id='about' className='w-full px-[12%] py-10 scroll-mt-20'>
+        <motion.h4
+        initial={{opacity: 0, y: -20}}
+        whileInView={{opacity: 1, y: 0}}
+        transition={{duration: 0.5, delay: 0.3}}
+        className='text-center mb-2 text-lg font-Ovo'>Introduction</motion.h4>
+        <motion.h2
+        initial={{opacity: 0, y: -20}}
+        whileInView={{opacity: 1, y: 0}}
+        transition={{duration: 0.5, delay: 0.5}}
+        className='text-center text-5xl font-Ovo'>About me</motion.h2>
 
-        <div className='flex w-full flex-col lg:flex-row items-center gap-20 my-20'>
-            <div className='w-80 sm:w-80 rounded-3xl max-w-none flex-1'>
+        <motion.div
+        initial={{opacity: 0}}
+        whileInView={{opacity: 1}}
+        transition={{duration: 0.8}}
+        className='flex w-full flex-col lg:flex-row items-center gap-20 my-20'>
+            <motion.div
+            initial={{opacity: 0, scale: 0.9}}
+            whileInView={{opacity: 1, scale: 1}}
+            transition={{duration: 0.6}}
+            className='w-80 sm:w-80 rounded-3xl max-w-none flex-1'>
                 <Image src={assets.user_image} alt='user' className='w-full rounded-3xl' />
-            </div>
-            <div className='flex-1'>
+            </motion.div>
+            <motion.div
+            initial={{opacity: 0}}
+            whileInView={{opacity: 1}}
+            transition={{duration: 0.6, delay: 0.8}}
+            className='flex-1'>
                 <p className='mb-10 max-w-2xl font-Ovo'>
                     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias, totam quo quae officia suscipit cum accusantium porro id consequuntur dolores ipsum nulla! Perferendis dolores est incidunt optio magnam officiis id.
                 </p>
-                <ul className='grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl'>
+                <motion.ul
+                initial={{opacity: 0}}
+                whileInView={{opacity: 1}}
+                transition={{duration: 0.8, delay: 1}}
+                className='grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl'>
                     {infoList.map(({icon, iconDark, title, description}, index) => (
-                        <li key={index} className='border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 hover:shadow-(--box-shadow-black) dark:border-white dark:hover:shadow-(--box-shadow-white) dark:hover:bg-darkHover/50'>
+                        <motion.li
+                        whileHover={{scale: 1.05}}
+                        key={index} className='border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 hover:shadow-(--box-shadow-black) dark:border-white dark:hover:shadow-(--box-shadow-white) dark:hover:bg-darkHover/50'>
                             <Image src={ isDarkMode ? iconDark : icon} alt={title}/>
                             <h3 className='my-4 font-semibold text-gray-700 font-Outfit dark:text-white'>{title}</h3>
                             <p className='text-gray-600 text-sm dark:text-white/80'>{description}</p>
-                        </li>
+                        </motion.li>
                     ))}
-                </ul>
-                <h4 className='my-6 text-gray-700 font-Ovo dark:text-white/80'>Tools I use</h4>
-                <ul className='flex items-center gap-3 sm:gap-5'>
+                </motion.ul>
+                <motion.h4 
+                initial={{y: 20, opacity: 0}}
+                whileInView={{y: 0, opacity: 1}}
+                transition={{duration: 0.6, delay: 1.5}}
+                className='my-6 text-gray-700 font-Ovo dark:text-white/80'>Tools I use</motion.h4>
+                <motion.ul
+                initial={{opacity: 0}}
+                whileInView={{opacity: 1}}
+                transition={{duration: 0.8, delay: 1}}
+                className='flex items-center gap-3 sm:gap-5'>
                     {toolsData.map((tool, index) => (
-                        <li className='flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500 ' key={index} >
+                        <motion.li 
+                        whileHover={{ scale: 1.1 }}
+                        className='flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500 ' key={index} >
                             <Image src={tool} alt='' className='w-5 sm:w-7'/>
                             <span>{tool.name}</span>
-                        </li>
+                        </motion.li>
                     ))}
-                </ul>
-            </div>
-        </div>
-    </div>
+                </motion.ul>
+            </motion.div>
+        </motion.div>
+    </motion.div>
   )
 }
 
--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -1,19 +1,44 @@
 import { assets, workData } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
+import { motion } from "motion/react"
+
 
 const Work = ({isDarkMode}) => {
 return (
-    <div id='work' className='w-full px-[12%] py-10 scroll-mt-20'>
-        <h4 className='text-center mb-2 text-lg font-Ovo'>My portfolio</h4>
-        <h2 className='text-center text-5xl font-Ovo'>My latest work</h2>
-        <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
+    <motion.div
+    initial={{opacity: 0}}
+    whileInView={{opacity: 1}}
+    transition={{duration: 1}}
+    id='work' className='w-full px-[12%] py-10 scroll-mt-20'>
+        <motion.h4
+        initial={{opacity: 0, y: -20}}
+        whileInView={{opacity: 1, y: 0}}
+        transition={{duration: 0.5, delay: 0.3}}
+        className='text-center mb-2 text-lg font-Ovo'>My portfolio</motion.h4>
+        <motion.h2
+        initial={{opacity: 0, y: -20}}
+        whileInView={{opacity: 1, y: 0}}
+        transition={{duration: 0.5, delay: 0.5}}
+        className='text-center text-5xl font-Ovo'>My latest work</motion.h2>
+        <motion.p
+        initial={{opacity: 0}}
+        whileInView={{opacity: 1}}
+        transition={{duration: 0.5, delay: 0.7}}
+        className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
             A showcase of my web development projects, from e-commerce platforms to personal blogs. Explore my work and let's collaborate!
-        </p>
+        </motion.p>
 
-        <div className='grid grid-cols-(--grid-cols-auto) gap-5 my-10 dark:text-black'>
+        <motion.div
+        initial={{opacity: 0}}
+        whileInView={{opacity: 1}}
+        transition={{duration: 0.6, delay: 0.9}}
+        className='grid grid-cols-(--grid-cols-auto) gap-5 my-10 dark:text-black'>
             {workData.map((project, index) =>(
-                <div key={index} className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group' style={{backgroundImage: `url(${project.bgImage})`}}>
+                <motion.div
+                whileHover={{ scale: 1.05 }}
+                transition={{ duration: 0.3 }}
+                key={index} className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group' style={{backgroundImage: `url(${project.bgImage})`}}>
                     <div className='bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7'>
                         <div>
                             <h2 className='font-semibold'>{project.title}</h2>
@@ -24,11 +49,15 @@ return (
                         </div> 
                     </div>
                     
-                </div>
+                </motion.div>
             ))}
-        </div>
-        <a href="" className='w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500 dark:text-white dark:border-white dark:hover:bg-darkHover'>Show more <Image src={ isDarkMode ? assets.right_arrow_bold_dark : assets.right_arrow_bold} alt='Right arrow' className='w-4' /> </a>
-    </div>
+        </motion.div>
+        <motion.a
+        initial={{opacity: 0}}
+        whileInView={{opacity: 1}}
+        transition={{duration: 0.5, delay: 1.1}}
+        href="" className='w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500 dark:text-white dark:border-white dark:hover:bg-darkHover'>Show more <Image src={ isDarkMode ? assets.right_arrow_bold_dark : assets.right_arrow_bold} alt='Right arrow' className='w-4' /> </motion.a>
+    </motion.div>
 )
 }
 
